2f3144ac1ee8a33bbe4f17c09140a17d820b7f88
Ferdaus0001 <144084398+Ferdaus0001@users.noreply.github.com>
2026-09-22 10:15:46 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/master_interview/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.master_interview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HoemScreen extends StatefulWidget {
+  const HoemScreen({super.key});
+
+  @override
+  State<HoemScreen> createState() => _HoemScreenState();
+}
+
+class _HoemScreenState extends State<HoemScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
